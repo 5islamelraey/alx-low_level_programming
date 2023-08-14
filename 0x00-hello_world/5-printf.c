d14288e2df909c44026172a7cd11fe0e4c08c126
5islamelraey <elraeyislam11@gmail.com>
2023-08-14 15:58:40 -0700

    0x00-hello_world

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Start the pro
+ * Discription: print a line and break with a new line
+ * Return: Return 0
+*/
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
