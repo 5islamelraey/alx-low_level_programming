3c3182f4e2f022e0238c9c52075d583fc133dc8c
5islamelraey <elraeyislam11@gmail.com>
2023-08-13 17:18:16 -0700

    0x00-hello_world

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - The entry point
+ *
+ * Description: A C pro that print a statment"/
+ * 
+ * Return: Always return 0
+*/
+
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
