dc0008a2162e29884ff034b05a0e30e7282beaec
5islamelraey <elraeyislam11@gmail.com>
2023-08-15 19:05:33 -0700

    0x02-functions_nested_loops

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -2,10 +2,8 @@
 
 /**
  * print_sign - prints the sign of a number
- *
  * @n: prints the sign of a number
- *
- * Retuen: 1 if + , 0 if 0, -1 if -
+ * Return: 1 if + , 0 if 0, -1 if -
 */
 
 int print_sign(int n)
