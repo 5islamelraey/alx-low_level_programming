e01a4c8b90d2fdf49fa76347a1495e3b0573397d
5islamelraey <elraeyislam11@gmail.com>
2023-08-15 20:08:10 -0700

    0x02-functions_nested_loops

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -13,7 +13,7 @@ void print_to_98(int num)
 		for (count = num; count > 98; count--)
 			printf("%d, ", count);
 	else
-		for (count = num; count > 98; count++)
+		for (count = num; count < 98; count++)
 			printf("%d, ", count);
 	printf("98\n");
 }
