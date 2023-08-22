40ffe71c1c960b8e4347bbb122f9c47f6d1b9112
5islamelraey <elraeyislam11@gmail.com>
2023-08-21 17:16:40 -0700

    0x05-pointers_arrays_strings

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_rev - prints a string, in reverse, followed by a new line
+ * @s: prameter
+*/
+
+void print_rev(char *s)
+{
+	int rev = 0;
+
+	while (s[rev])
+		rev++;
+
+	while (rev--)
+		_putchar(s[rev]);
+	_putchar('\n');
+}
